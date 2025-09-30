9e95635d86427f450a105ee51307e17a67fb1eb2
is.bot <168564255+IsraelBordones@users.noreply.github.com>
2025-09-30 10:20:04 -0300

    backend
    carpetas agregadas firebase para backend y conexión con bases de datos firebase, también usuario creado

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA1_om-_HPyYVnUo8ELiM5Zob2VSMGbWvw",
+    authDomain: "pasteleriamilsaborestresleches.firebaseapp.com",
+    projectId: "pasteleriamilsaborestresleches",
+    storageBucket: "pasteleriamilsaborestresleches.firebasestorage.app",
+    messagingSenderId: "724534518591",
+    appId: "1:724534518591:web:fda9e47afb93ed6854e98a",
+    measurementId: "G-FXQWCCHM83"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
